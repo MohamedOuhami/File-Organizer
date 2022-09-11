56f41d031c72882bd129694260ae797915f88499
Mohamed Ouhami <mohamed.ouhami2001@gmail.com>
2022-09-11 16:23:28 +0100

    Sorting the files according to Its extension

--- fileorganizer.py
+++ fileorganizer.py
@@ -1,28 +1,38 @@
 import os
 import shutil
+import pathlib
 
 # Setting the designed file to transfer
-# As far I just want to transfer 1 file to my music folder
-file_source = r"/home/mohamed/Downloads/"
-folder_dest = r"/home/Documents/Music/"
+# Detect the file extension
 
-print("Here are your files : ")
+# Before you can work with this file organizer you need to have the folders " Downloads ", " Images", "Videos", "Music" added
+# If you know what you're doing, you can change this code according to your needs
 
-def fetching_files(src,dst):
-    for file in os.listdir(src): # Fetching all of the files presented in the source path
-        source = os.path.join(src, file)
-        destination = os.path.join(dst, file)
-
-        print(file)
-
-def copying_files(src,dst):
+def copying_files(src,dst,file):
     
-
-    for file in os.listdir(src): # Fetching all of the files presented in the source path
         source = os.path.join(src, file)
         destination = os.path.join(dst, file)
 
         shutil.move(source, destination)
 
-fetching_files('Downloads', 'destination')
-copying_files('Downloads', 'destination')
+def file_type(src):
+    for file in os.listdir(src):
+        source = os.path.join(src, file)
+
+        if pathlib.Path(source).suffix == '.png': # Adding Images
+            copying_files('Downloads', 'Images',file)
+
+        elif pathlib.Path(source).suffix == '.mp3': # Adding Music
+            copying_files('Downloads', 'Music',file)
+
+        elif pathlib.Path(source).suffix == '.mp4': # Adding Vidos
+            copying_files('Downloads', 'Videos',file)
+
+        elif pathlib.Path(source).suffix == '.pdf': # Adding PDFs
+            copying_files('Downloads', 'PDFs',file)
+
+
+try :
+    file_type('Downloads')
+except:
+    print(" There is an error in the directories. Please check that you have the following folders in your root : Images, Videos, Music, PDFs")
